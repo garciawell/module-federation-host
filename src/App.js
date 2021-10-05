6e211cd5f79ca7dc6b0051a629f502802d32b10c
Wellington Garcia <garciawell@gmail.com>
2021-10-04 22:04:58 -0300

    feat: webpack config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// import Header from "header/Header";
+
+const App = () => (
+  <div>
+    {/* <Header /> */}
+    <div>HOST APP</div>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
