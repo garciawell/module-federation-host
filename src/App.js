4cb11869b2cfc3d64ea553c1ae0dec8b0f988531
Wellington Garcia <garciawell@gmail.com>
2021-10-04 23:38:51 -0300

    feat: federation with hot reloading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import LocalComponent from "./components/LocalComponent";
 import GlobalStyle from "./styles/Globalnject";
 
 
 const Header = React.lazy(() => import("children/Header"));
 const Footer = React.lazy(() => import("children/Footer"));
 
-const App = () => (
+const App = () => {
+
+
+  return(
   <div>
       <React.Suspense fallback="Loading Name">
         <Header />
       </React.Suspense>
     <div>
-      <h1>HOST APP 1</h1>
+      <h1>HOST 222</h1>
     </div>
     <React.Suspense fallback="Loading Name">
       <Footer/>
     </React.Suspense>
+    <LocalComponent />  
     <GlobalStyle />
   </div>
-);
+  )
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- src/styles/Globalnject.js
+++ src/styles/Globalnject.js
@@ -7,7 +7,7 @@ const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        background: #bbb; 
+        background: #ddd; 
         font-family: 'Open-Sans', Helvetica, Sans-Serif;
     }
 `;
